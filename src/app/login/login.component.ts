9d019bbf6bea586a109527c60da97c5bd68cbba5
visidevi <32283668+visidevi@users.noreply.github.com>
2020-03-01 23:27:26 -0300

    estilos de nuestra pantalla de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  operation: string = "login"
   constructor() { }
 
   ngOnInit(): void {
