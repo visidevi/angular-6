b6fa0768f635ed25e159b652a89fcc20c21033e8
visidevi <32283668+visidevi@users.noreply.github.com>
2020-03-23 00:52:33 -0300

    Guards para verificar Firebase auth

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../interfaces/user'
 import { UserService } from '../services/user.service';
+import { AuthenticationService } from '../services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +12,9 @@ import { UserService } from '../services/user.service';
 export class HomeComponent implements OnInit {
   friends: User[]
   query: string = '';
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService,
+    private authenticationService: AuthenticationService,
+    private router: Router ) {
     this.userService.getUsers()
       .valueChanges()
       .subscribe((data: User[]) => {
@@ -19,6 +23,12 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+  logout() {
+    this.authenticationService.logOut().then((data) => {
+      this.router.navigate(['login'])
+      console.log(data)
+    }). catch((e) => {console.log(e)})
   }
 
 }
---
+++ src/app/services/authentication.guard.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanActivateChild, CanDeactivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from './authentication.service';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate, CanActivateChild, CanDeactivate<unknown>, CanLoad {
+
+  constructor(private authenticationService: AuthenticationService,
+    private router: Router) {
+
+  }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authenticationService.getStatus().pipe(
+      map(status => {
+        if (!status) {
+          this.router.navigate(['login'])
+          return false
+        }
+        return true
+      })
+    )
+  }
+  canActivateChild(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  canDeactivate(
+    component: unknown,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    return true;
+  }
+}
